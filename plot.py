043ab860d521ffe2b3d183a438b9cf343c0cf810
Narxem <adrien.agez@hotmail.fr>
2016-01-27 17:23:20 +0100

    initial

---
+++ plot.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from math import sqrt
+
+### read datas
+p = open("p.txt", "r")
+t = open("t.txt", "r")
+p = np.loadtxt('p.txt')
+t = np.loadtxt('t.txt')
+###
+
+def f(x):
+    return 2*x + 3
+
+theta = np.array([2, 3])
+N = 100.
+x = np.array([np.array([i, 1]) for i in t]).T
+print(x)
+print(np.shape(x))
+
+jl2 = np.dot((p - np.dot(x.T,theta)).T, (p - np.dot(x.T, theta))) * (1./100)
+print(jl2)
+
+### Display datas
+plt.plot(t, p, 'r.')
+plt.plot(range(5, 16), [f(x) for x in range(5, 16)], "g")
+plt.xlabel('time (sec)')
+plt.ylabel('position (m)')
+#plt.show()
+
