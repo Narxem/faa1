c9960b9b6a493095a22bfcd7538dd2a9aeec1971
Kaendan <tristan.camus@gmail.com>
2016-02-02 15:59:46 +0100

    ajout de jl1, jlabs, jlinf et des moindres carrés.

--- plot.py
+++ plot.py
@@ -15,17 +15,34 @@ def f(x):
 theta = np.array([2, 3])
 N = 100.
 x = np.array([np.array([i, 1]) for i in t]).T
-print(x)
-print(np.shape(x))
 
-jl2 = np.dot((p - np.dot(x.T,theta)).T, (p - np.dot(x.T, theta))) * (1./100)
-print(jl2)
+jlabs = np.sum(np.absolute(p - theta.T.dot(x))) / N
+jl2 = np.dot((p - np.dot(x.T,theta)).T, (p - np.dot(x.T, theta))) * (1./N)
+jl1 = sqrt(jl2 * N) / N
+jlinf = np.max(np.absolute(p - theta.T.dot(x)))
+
+print "TP1"
+print "jlabs =",jlabs
+print "jl1 =",jl1
+print "jl2 =",jl2
+print "jlinfini =",jlinf
+
+moindre_carre = np.linalg.inv(x.dot(x.T)).dot(x.dot(p))
+
+print "Moindre Carres"
+jlabs_mc = np.sum(np.absolute(p - moindre_carre.T.dot(x))) / N
+jl2_mc = np.dot((p - np.dot(x.T,moindre_carre)).T, (p - np.dot(x.T, moindre_carre))) * (1./N)
+jl1_mc = sqrt(jl2_mc * N) / N
+jlinf_mc = np.max(np.absolute(p - moindre_carre.T.dot(x)))
+
+print "jlabs =",jlabs_mc
+print "jl1 =",jl1_mc
+print "jl2 =",jl2_mc
+print "jlinfini =",jlinf_mc
 
 ### Display datas
 plt.plot(t, p, 'r.')
 plt.plot(range(5, 16), [f(x) for x in range(5, 16)], "g")
 plt.xlabel('time (sec)')
 plt.ylabel('position (m)')
-#test
-#plt.show()
-
+plt.show()
