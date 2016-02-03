586cce8ff47c5be1211d417d43ef5a94fe557249
Kaendan <tristan.camus@gmail.com>
2016-02-03 13:44:01 +0100

    correction de bug et ajout d'affichages

--- plot.py
+++ plot.py
@@ -21,24 +21,24 @@ jl2 = np.dot((p - np.dot(x.T,theta)).T, (p - np.dot(x.T, theta))) * (1./N)
 jl1 = sqrt(jl2 * N) / N
 jlinf = np.max(np.absolute(p - theta.T.dot(x)))
 
-print "TP1"
-print "jlabs =",jlabs
-print "jl1 =",jl1
-print "jl2 =",jl2
-print "jlinfini =",jlinf
+print "TP1", theta
+print "jlabs =", jlabs
+print "jl1 =", jl1
+print "jl2 =", jl2
+print "jlinfini =", jlinf
 
 moindre_carre = np.linalg.inv(x.dot(x.T)).dot(x.dot(p))
 
-print "Moindre Carres"
+print "Moindre Carres", moindre_carre
 jlabs_mc = np.sum(np.absolute(p - moindre_carre.T.dot(x))) / N
 jl2_mc = np.dot((p - np.dot(x.T,moindre_carre)).T, (p - np.dot(x.T, moindre_carre))) * (1./N)
 jl1_mc = sqrt(jl2_mc * N) / N
 jlinf_mc = np.max(np.absolute(p - moindre_carre.T.dot(x)))
 
-print "jlabs =",jlabs_mc
-print "jl1 =",jl1_mc
-print "jl2 =",jl2_mc
-print "jlinfini =",jlinf_mc
+print "jlabs =", jlabs_mc
+print "jl1 =", jl1_mc
+print "jl2 =", jl2_mc
+print "jlinfini =", jlinf_mc
 
 ### Display datas
 plt.plot(t, p, 'r.')
