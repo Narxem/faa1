af1c742c519f80e856d9511cef120f4c4ab8990b
Narxem <adrien.agez@hotmail.fr>
2016-02-21 18:43:25 +0100

    don't use global var in functions

--- plot.py
+++ plot.py
@@ -22,60 +22,57 @@ N = 100.
 x = np.array([np.array([i, 1]) for i in t]).T
 
 alpha = 0.001
-MAX_IT = 10
+MAX_IT = 1000
 
 #################
 
-def jlabs() :
-	return np.sum(np.absolute(p - theta.T.dot(x))) / N
+def jlabs(x, y, theta, N) :
+	return np.sum(np.absolute(y - theta.T.dot(x))) / N
 
-def jl2() :
-	return np.dot((p - np.dot(x.T,theta)).T, (p - np.dot(x.T, theta))) * (1./N)
+def jl2(x, y, theta, N) :
+	return np.dot((y - np.dot(x.T,theta)).T, (y - np.dot(x.T, theta))) * (1./N)
 
-def jl1() :
-	return sqrt(jl2() * N) / N
+def jl1(x, y, theta, N) :
+	return sqrt(jl2(x, y, theta, N) * N) / N
 
-def jlinf() :
-	return np.max(np.absolute(p - theta.T.dot(x)))
+def jlinf(x, y, theta) :
+	return np.max(np.absolute(y - theta.T.dot(x)))
 
-def moindre_carre() :
-   return np.linalg.inv(x.dot(x.T)).dot(x.dot(p))
+def moindre_carre(x, y) :
+   return np.linalg.inv(x.dot(x.T)).dot(x.dot(y))
 
-def jlabs_mc() :
-	return np.sum(np.absolute(p - moindre_carre().T.dot(x))) / N
+def jlabs_mc(x, y, N) :
+	return np.sum(np.absolute(p - moindre_carre(x, y).T.dot(x))) / N
 
-def jl2_mc() :
-	return np.dot((p - np.dot(x.T,moindre_carre())).T, (p - np.dot(x.T, moindre_carre()))) * (1./N)
+def jl2_mc(x, y, N) :
+	return np.dot((y - np.dot(x.T,moindre_carre(x, y))).T, (y - np.dot(x.T, moindre_carre(x, y)))) * (1./N)
 
-def jl1_mc() :
-	return sqrt(jl2_mc() * N) / N
+def jl1_mc(x, y, N) :
+	return sqrt(jl2_mc(x, y, N) * N) / N
 
-def jlinf_mc() :
-	return np.max(np.absolute(p - moindre_carre().T.dot(x)))
+def jlinf_mc(x, y) :
+	return np.max(np.absolute(y - moindre_carre(x, y).T.dot(x)))
 
-def descenteGradiant() :
-	theta = np.array([10., 8.])
+def descenteGradiant(x, y, alpha, theta, nbIter) :
 	tab = [theta]
-
 	for i in range(1, 1000):
-		theta = theta + alpha / N * np.dot(x, p - np.dot(x.T, theta))
-		print(theta)
+		theta = theta + alpha / N * np.dot(x, y - np.dot(x.T, theta))
 		tab.append(theta)
 	return tab
 
 #### Prints #####
 
 print "TP1", theta
-print "jlabs =", jlabs()
-print "jl1 =", jl1()
-print "jl2 =", jl2()
-print "jlinfini =", jlinf()
+print "jlabs =", jlabs(x, p, theta, N)
+print "jl1 =", jl1(x, p, theta, N)
+print "jl2 =", jl2(x, p, theta, N)
+print "jlinfini =", jlinf(x, p, theta)
 
-print "Moindre Carres", moindre_carre()
-print "jlabs =", jlabs_mc()
-print "jl1 =", jl1_mc()
-print "jl2 =", jl2_mc()
-print "jlinfini =", jlinf_mc()
+print "Moindre Carres", moindre_carre(x, p)
+print "jlabs =", jlabs_mc(x, p, N)
+print "jl1 =", jl1_mc(x, p, N)
+print "jl2 =", jl2_mc(x, p, N)
+print "jlinfini =", jlinf_mc(x, p)
 
 #################
 
@@ -85,7 +82,7 @@ print "jlinfini =", jlinf_mc()
 #plt.plot(t, p, 'ro')
 #plt.plot(range(5, 16), [f(x) for x in range(5, 16)], "g", label="2x+3")
 #plt.plot(range(5,16), [x*moindre_carre[0]+moindre_carre[1] for x in range(5,16)], "b", label="moindre carre")
-DG = descenteGradiant() 
+DG = descenteGradiant(x, p, alpha, [10., 8.], MAX_IT) 
 plt.plot(range(0, MAX_IT), [DG[i][0] for i in range(0, MAX_IT)], "r")
 plt.plot(range(0, MAX_IT), [DG[i][1] for i in range(0, MAX_IT)], "g")
 #plt.plot(t, np.dot(x.T, theta), "g")
